2e1ee5ec1f4f8d1c053d16e044ac2b1c7d78bd7c
fleaz <mail@felixbreidenstein.de>
2024-12-03 00:51:50 +0100

    Enhance newDay.sh script

--- newDay.sh
+++ newDay.sh
@@ -1,11 +1,19 @@
 #! /usr/bin/env bash
+set -eu
 
-DAY="$( printf '%02d' "$1" )"
-FOLDER="day$DAY"
+DAY=$1
+LONGDAY="$( printf '%02d' "$1" )"
+FOLDER="day$LONGDAY"
+YEAR=`date "+%Y"`
 
-mkdir $FOLDER
-touch $FOLDER/input.txt
-cat <<EOT >> $FOLDER/main.go
+mkdir -p $YEAR/$FOLDER
+echo "Created $YEAR/$FOLDER"
+
+COOKIE=`cat session.txt`
+curl -s --compressed -H "Cookie: $COOKIE" "https://adventofcode.com/$YEAR/day/$DAY/input" > $YEAR/$FOLDER/input.txt
+echo "Downloadedd input.txt"
+
+cat <<EOT >> $YEAR/$FOLDER/main.go
 package main
 
 import (
@@ -20,34 +28,8 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-        fmt.Println(scanner.Text()
+        fmt.Println(scanner.Text())
 	}
 }
 EOT
-
-cat <<EOT >> $FOLDER/main_test.go
-package main
-
-import "testing"
-
-type testpair struct {
-	value  int
-	want   int
-}
-
-func Test(t *testing.T) {
-	pairs := []testpair{
-		{,},
-		{,},
-		{,},
-	}
-
-	for _, p := range pairs {
-        is := functioncall()
-		if is != p.want {
-			t.Errorf("Wanted %d but got %d", p.want, is)
-		}
-
-	}
-}
-EOT
+echo "Started main.go"
